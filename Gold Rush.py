67dcdbcd26b75d9732014099b483b1b1c0fb96be
KratkyS <kratkys24@spstrutnov.cz>
2025-01-31 13:33:31 +0100

    nastavení okna aplikace

---
+++ Gold Rush.py
@@ -0,0 +1,11 @@
+import random
+import pygame
+pygame.init()
+
+#okno aplikace
+resolution_x = 800
+resolution_y = 600
+
+window = pygame.display.set_mode((resolution_x, resolution_y))
+pygame.display.set_caption('Gold Rush')
+
