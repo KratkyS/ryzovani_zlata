420d320032fd505ef14f5253a4a709f9c8dfa115
KratkyS <kratkys24@spstrutnov.cz>
2025-01-31 14:08:07 +0100

    Přidání pozadí a složky na obrázky
    za pomoci knihovny os, se otevře složka images a obrázek background.png, který se následně vykreslí do objektu se stejnou velikostí, jako obrázek.

--- Gold Rush.py
+++ Gold Rush.py
@@ -1,4 +1,5 @@
 import random
+import os
 import pygame
 pygame.init()
 
@@ -9,3 +10,32 @@ resolution_y = 600
 window = pygame.display.set_mode((resolution_x, resolution_y))
 pygame.display.set_caption('Gold Rush')
 
+#lobby
+Lobby = True
+image_path = os.path.join("images", "background.png")
+background = pygame.image.load(image_path).convert_alpha()
+objekt = pygame.Surface(background.get_size())
+
+try:
+    background = pygame.image.load(image_path).convert_alpha()  # Přejmenováno na 'background'
+    objekt = pygame.Surface(background.get_size())
+    objekt.blit(background, (0, 0))
+except FileNotFoundError:
+    print(f"Soubor s obrázkem nebyl nalezen: {image_path}")
+    pygame.quit()
+    exit()
+     
+lobby = True
+while lobby:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            lobby = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                lobby = False
+            
+    window.blit(objekt, (0, 0))  # Vykreslení objektu s pozadím
+    pygame.display.flip()
+    
+
+pygame.quit()
