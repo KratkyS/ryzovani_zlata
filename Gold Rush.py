0fb4ff2368790d063310974bbf685f84317143ed
KratkyS <kratkys24@spstrutnov.cz>
2025-02-14 22:10:35 +0100

    příprava na tvorbu minihry těžby hlíny
    změna scény na které se nachází počet peněz a počet hlíny

--- Gold Rush.py
+++ Gold Rush.py
@@ -14,6 +14,11 @@ pygame.display.set_caption('Gold Rush')
 image_path_lobby = os.path.join("images", "background.png")
 background = pygame.image.load(image_path_lobby).convert_alpha()
 objekt_lobby = pygame.Surface(background.get_size())
+#tlačítko
+button_path_lobby = os.path.join("images", "button_image.png")
+button_image = pygame.image.load(button_path_lobby).convert_alpha()
+button_rect = button_image.get_rect(topleft=(600, -15))  # Nastavení pozice tlačítka
+
 
 # Dirt minihra
 image_path_dirt = os.path.join("images", "dirt_minigame.png")
@@ -27,6 +32,8 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 dirt_color = red
 dirt_count = 0
+button_position = (600, -15)
+mouse_x, mouse_y = 0, 0
 
 Game = True
 while Game:
@@ -43,6 +50,12 @@ while Game:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos  # Souřadnice kliknutí
+                if button_rect.collidepoint(mouse_x, mouse_y):  # Kontrola kliknutí na tlačítko
+                    print("Tlačítko bylo kliknuto!")
+                    lobby = False
+                    dirt_minigame = True
                 
 
         # Vykreslení lobby
@@ -57,6 +70,7 @@ while Game:
         window.blit(objekt_lobby, (0, -100))  
         window.blit(money, (10, 10))  
         window.blit(dirt, (300, 10))
+        window.blit(button_image, (button_position))
         pygame.display.flip()
 
     # DIRT_MINIGAME 
@@ -65,9 +79,28 @@ while Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                   dirt_minigame = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                   dirt_count += 10
+            
+                        
            
 
         # Vykreslení dirt minihry
-        window.blit(background_dirt, (0, -200)) 
+        money = font.render(f'Money: {money_count}', True, (255, 0, 0))
+        dirt = font.render(f'Dirt: {dirt_count}/100', True, dirt_color)
+        if dirt_count > 99:
+            dirt_color = green
+            dirt_count = 100
+            dirt_minigame = False
+
+        window.blit(background_dirt, (0, -200))
+        window.blit(money, (10, 10))
+        window.blit(dirt, (300, 10))
+        
         pygame.display.flip()
+        
 
