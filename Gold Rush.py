38e73e160296fd6e93b0e6f373ced4ca9b83719f
KratkyS <kratkys24@spstrutnov.cz>
2025-02-16 21:29:11 +0100

    Funkční minihra sběru hlíny
    po kliknutí na tlačítko mine dirt, změní se pozadí a zobrazí se 3 elipsy u kterých se po kliknutí přidá skóre/počet hlíny. Pokud hráč nasbírá 100 hlíny vrátí ho to na hlavní obrazovku/lobby a už minihra sběru nelze spustit

--- Gold Rush.py
+++ Gold Rush.py
@@ -24,6 +24,23 @@ button_rect = button_image.get_rect(topleft=(600, -15))  # Nastavení pozice tla
 image_path_dirt = os.path.join("images", "dirt_minigame.png")
 background_dirt = pygame.image.load(image_path_dirt).convert_alpha()
 objekt_dirt = pygame.Surface(background_dirt.get_size())
+dirt_count = 3
+dirt_size = 100
+dirt_x = []
+dirt_y = []
+dirt_rects = []
+dirt_value = 2
+dirt_object_color = (150, 75, 0)
+
+
+
+for index in range(dirt_count):
+    dirt_x.append(random.randint(10, 600))
+    dirt_y.append(random.randint(10, 600))
+    dirt_rects.append(pygame.Rect(dirt_x[index], dirt_y[index], dirt_size, dirt_size))
+    
+
+
 
 # Herní proměnné
 money_count = 0
@@ -34,6 +51,7 @@ dirt_color = red
 dirt_count = 0
 button_position = (600, -15)
 mouse_x, mouse_y = 0, 0
+
 
 Game = True
 while Game:
@@ -53,9 +71,11 @@ while Game:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos  # Souřadnice kliknutí
                 if button_rect.collidepoint(mouse_x, mouse_y):  # Kontrola kliknutí na tlačítko
-                    print("Tlačítko bylo kliknuto!")
+                    print("Tlačítko bylo stisknuto!")
                     lobby = False
                     dirt_minigame = True
+                    dirt_render = True
+                    
                 
 
         # Vykreslení lobby
@@ -75,6 +95,8 @@ while Game:
 
     # DIRT_MINIGAME 
     while dirt_minigame:
+        
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -82,11 +104,10 @@ while Game:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                    dirt_minigame = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                   dirt_count += 10
             
-                        
+            
+            
+           
            
 
         # Vykreslení dirt minihry
@@ -96,10 +117,29 @@ while Game:
             dirt_color = green
             dirt_count = 100
             dirt_minigame = False
-
+        
         window.blit(background_dirt, (0, -200))
         window.blit(money, (10, 10))
         window.blit(dirt, (300, 10))
+        
+        mouse_rect = pygame.Rect(mouse_x, mouse_y, 1, 1)
+        
+        for index in range(len(dirt_x)):
+          pygame.draw.ellipse(window, (dirt_object_color), (dirt_x[index], dirt_y[index], dirt_size, dirt_size))
+          
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            for index in range(len(dirt_rects)-1, -1, -1):  
+                if dirt_rects[index].colliderect(mouse_rect):  
+                        dirt_count += dirt_value
+                        del dirt_x[index]
+                        del dirt_y[index]
+                        del dirt_rects[index]
+                        dirt_x.append(random.randint(10, 600))
+                        dirt_y.append(random.randint(10, 600))
+                        dirt_rects.append(pygame.Rect(dirt_x[-1], dirt_y[-1], dirt_size, dirt_size))
+                        break
+                        
         
         pygame.display.flip()
         
