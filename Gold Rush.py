dfb9fa86910a85787b092b01ae5c1b8e0075e976
KratkyS <kratkys24@spstrutnov.cz>
2025-03-01 17:24:31 +0100

    příprava na minihru rýžování
    tlačítko pro přechod mezi lobby a minihrou, odečtení počtu hlíny po startu minihry

--- Gold Rush.py
+++ Gold Rush.py
@@ -19,7 +19,16 @@ button_path_lobby = os.path.join("images", "button_image.png")
 button_image = pygame.image.load(button_path_lobby).convert_alpha()
 button_rect = button_image.get_rect(topleft=(600, -15))  # Nastavení pozice tlačítka
 
-  
+#tlačítko 2
+button2_path_lobby = os.path.join("images", "Gold_panning_button.png")
+button2_image = pygame.image.load(button2_path_lobby).convert_alpha()
+button2_rect = button2_image.get_rect(topleft=(610, 175))
+resized_button2 = pygame.transform.scale(button2_image, (175, 175))
+
+#minihra zlata
+image_path_gold = os.path.join("images", "Gold_panning_background.png")
+background_gold = pygame.image.load(image_path_gold).convert_alpha()
+resized_gold_background = pygame.transform.scale(background_gold, (800, 700)) 
 
 # Dirt minihra
 image_path_dirt = os.path.join("images", "dirt_minigame.png")
@@ -58,6 +67,7 @@ green = (0, 255, 10)
 dirt_color = red
 dirt_count = 0
 button_position = (600, -15)
+button2_position = (610, 175)
 mouse_x, mouse_y = 0, 0
 
 # Časování pro popcorn
@@ -66,10 +76,12 @@ popcorn_duration = 500  # Délka zobrazení v milisekundách (2 sekundy)
 popcorn_visible2 = False # Určuje, zda je popcorn text viditelný
 popcorn_visible1 = False
 Game = True
+ 
+
 while Game:
     lobby = True
-    dirt_minigame = False  
-
+    dirt_minigame = False
+    gold_minigame = False
     # LOBBY SMYČKA
     while lobby:
         for event in pygame.event.get():
@@ -87,6 +99,15 @@ while Game:
                     lobby = False
                     dirt_minigame = True
                     dirt_render = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos  # Souřadnice kliknutí
+                if button2_rect.collidepoint(mouse_x, mouse_y):
+                    if dirt_count >99:
+                        print("Tlačítko bylo stisknuto!")
+                        lobby = False
+                        gold_minigame = True
+                        
+                    
                  
 
         # Vykreslení lobby
@@ -102,6 +123,7 @@ while Game:
         window.blit(money, (10, 10))  
         window.blit(dirt, (300, 10))
         window.blit(button_image, (button_position))
+        window.blit(resized_button2, (button2_position))
         pygame.display.flip()
 
     # DIRT_MINIGAME 
@@ -165,4 +187,25 @@ while Game:
         if popcorn_visible1:
             popcorn_minus()
         pygame.display.flip()
+        
+#minihra rýžování zlata        
+    while gold_minigame:
+        dirt_count = 0
+        dirt_color = red
+       
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                   dirt_minigame = False
+        money = font.render(f'Money: {money_count}', True, (255, 0, 0))
+        dirt = font.render(f'Dirt: {dirt_count}/100', True, dirt_color)
+        window.blit(resized_gold_background, (0, 0))
+        window.blit(money, (10, 10))
+        window.blit(dirt, (300, 10))
+        pygame.display.flip()
+        
+        
 
